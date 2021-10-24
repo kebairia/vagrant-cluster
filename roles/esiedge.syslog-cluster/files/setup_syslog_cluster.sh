5287a31a60cf8508220767aa68e087e80f917cbd
Zakaria Kebairia <4.kebairia@gmail.com>
2021-10-24 21:05:12 +0100

    changing esiedge --> lab

--- roles/esiedge.syslog-cluster/files/setup_syslog_cluster.sh
+++ roles/esiedge.syslog-cluster/files/setup_syslog_cluster.sh
@@ -1,7 +1,7 @@
 echo "Setting password for user `hacluster`"
-echo esiedge | passwd --stdin hacluster
+echo lab | passwd --stdin hacluster
 echo "Authenticate the nodes"
-pcs host auth syslog1 syslog2 -u hacluster -p esiedge
+pcs host auth syslog1 syslog2 -u hacluster -p lab
 # pcs host auth "{{ pcs nodes }}" -u "{{ pcs_user }}" -p "{{ pcs_pass }}"
 # echo "Setup the cluster `"{{ cluster_name }}"`"
 # pcs cluster setup "{{ cluster_name }}"
